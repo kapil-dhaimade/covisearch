edc5739d7db0e378b6f741b65a386aeaf47e74c8
DESKTOP-RV2S9RR\Prateek <pdwivedi123321@gmail.com>
2021-05-18 23:39:02 +0530

    added moment

--- src/aggregation/portal/myScript.js
+++ src/aggregation/portal/myScript.js
@@ -40,6 +40,10 @@ app.controller('formCtrl', function($scope,$http) {
             console.log($scope.leads.data);
         });
         $scope.dataFetched=true;
+        var now =moment("2021-05-17#10:26:08.937881-06:00").fromNow();
+        $scope.date= now;
+        var str ='12345,23456,34,567';
+        $scope.numbers = str;
     }
     
     $scope.reset = function() {
