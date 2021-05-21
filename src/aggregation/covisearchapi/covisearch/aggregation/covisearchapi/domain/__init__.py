08713a48939e7aba137b01801fd25dbbd4c70c6d
doshisahil <sahil000005@gmail.com>
2021-05-21 15:47:25 +0530

    added code to invoke pubsub & update stats and few other changes

--- src/aggregation/covisearchapi/covisearch/aggregation/covisearchapi/domain/__init__.py
+++ src/aggregation/covisearchapi/covisearch/aggregation/covisearchapi/domain/__init__.py
@@ -27,7 +27,20 @@ def fetch_resource_for_filter(request: Request):
     # This is auto-initialized when main program loads
     db = firestore.Client()
 
+    # ======================updating stats======================
     res_info_filter_id = "city=" + city + "&resource-type=" + resource_type
+    filter_stat_doc = db.collection('filter-stats'). \
+        document(res_info_filter_id).get()
+
+    query_time = {"last-query-time-utc": DatetimeWithNanoseconds.today()}
+    if not filter_stat_doc.exists:
+        db.collection('filter-stats').add(query_time, "res_info_filter_id")
+        raise abort(202)
+    else:
+        db.collection('filter-stats'). \
+            document(res_info_filter_id).set(query_time)
+
+    # ======================fetching resource======================
     res_info_doc = db.collection('filtered-aggregated-resource-info'). \
         document(res_info_filter_id).get()
 
@@ -41,11 +54,19 @@ def fetch_resource_for_filter(request: Request):
         if len(resources) < (page_no - 1) * page_size:
             raise abort(400)
         else:
-            return json.dumps({"res-info-data": resources[(page_no - 1) * page_size:len(resources) - 1]},
-                              cls=DateTimeEncoder)
+            more_data_available = False
+            res_info_data = resources[(page_no - 1) * page_size:len(resources) - 1]
     else:
-        return json.dumps({"res-info-data": resources[(page_no - 1) * page_size:page_no * page_size]},
-                          cls=DateTimeEncoder)
+        more_data_available = True
+        res_info_data = resources[(page_no - 1) * page_size:page_no * page_size]
+
+    response_dict = {
+        "meta-info": {
+            "more-data-available": more_data_available,
+        },
+        "res-info-data": res_info_data
+    }
+    return json.dumps(response_dict, cls=DateTimeEncoder)
 
 
 # Publishes a message to a Cloud Pub/Sub topic.
