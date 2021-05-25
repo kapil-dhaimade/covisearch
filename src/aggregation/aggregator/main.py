01ddf42bd6646f0e13f203aad29e1ebba3a2e7f7
kapil-dhaimade <38310076+kapil-dhaimade@users.noreply.github.com>
2021-05-25 16:58:21 +0530

    -Added code in aggregator with performance improvements

--- src/aggregation/aggregator/main.py
+++ src/aggregation/aggregator/main.py
@@ -5,16 +5,24 @@ import google.cloud.firestore as firestore
 import covisearch.aggregation.core.infra as infra
 from covisearch.aggregation.core.domain import entities
 import covisearch.aggregation.core.domain as domain
+import covisearch.util.websitedatascraper as websitedatascraper
 
 
 # Keep this global as global vars are not reinitialized if same Cloud Function instance
 # is re-used. And we need DB init only once. Plus it is time intensive.
 db = firestore.Client()
 
+# NOTE: KAPIL: Parsing this dummy ISO format datetime during init improves performance of
+# subsequent datetime parsing operations and makes mapping websrc resources to covisearch faster!!
+dummy_datetime_for_performance = \
+    domain.resourcemapping._map_isoformat_timestamp_to_covisearch('2021-05-10T13:22:16.075259')
+
 
 # Starting point called by Google Cloud Function
 # Do init and call corresponding domain function
 def aggregate_covid_resources(event, context):
+    # NOTE: KAPIL: For performance reasons, spawn scrapy child process instantly on init.
+    websitedatascraper.start_scrapy_process_in_advance()
     aggregated_res_info_repo = infra.AggregatedResourceInfoRepoImpl(db)
     web_src_repo = infra.WebSourceRepoImpl(db)
     search_filter_str = base64.b64decode(event['data']).decode('utf-8')
@@ -23,10 +31,11 @@ def aggregate_covid_resources(event, context):
 
 
 # if __name__=='__main__':
+#     # city=mumbai&resource_type=ambulance
+#     aggregate_covid_resources({'data': 'Y2l0eT1tdW1iYWkmcmVzb3VyY2VfdHlwZT1hbWJ1bGFuY2U='}, None)
 #     # city=chennai&resource_type=plasma
 #     aggregate_covid_resources({'data': 'Y2l0eT1jaGVubmFpJnJlc291cmNlX3R5cGU9cGxhc21h'}, None)
 #     # # city=mumbai&resource_type=oxygen
 #     # aggregate_covid_resources({'data': 'Y2l0eT1tdW1iYWkmcmVzb3VyY2VfdHlwZT1veHlnZW4='}, None)
-#     # city=mumbai&resource_type=ambulance
-#     aggregate_covid_resources({'data': 'Y2l0eT1tdW1iYWkmcmVzb3VyY2VfdHlwZT1hbWJ1bGFuY2U='}, None)
+#
 #     print(9)
