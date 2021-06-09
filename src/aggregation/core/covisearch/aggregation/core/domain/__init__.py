2ad40d43f0d051f76c63e5c82f93691eb8f938e7
kapil-dhaimade <38310076+kapil-dhaimade@users.noreply.github.com>
2021-06-09 20:28:38 +0530

    -Fixed #36 -Fixed #37

--- src/aggregation/core/covisearch/aggregation/core/domain/__init__.py
+++ src/aggregation/core/covisearch/aggregation/core/domain/__init__.py
@@ -112,7 +112,7 @@ def _scrape_data_from_web_sources(web_sources: Dict[str, resourcemapping.WebSour
 #
 #         aggregated_res_info_repo = infra.AggregatedResourceInfoRepoImpl(db)
 #         web_src_repo = infra.WebSourceRepoImpl(db)
-#         search_filter = SearchFilter('nagercoil', entities.CovidResourceType.AMBULANCE, None)
+#         search_filter = SearchFilter('Kamrup Metropolitan', entities.CovidResourceType.AMBULANCE, None)
 #         aggregate_covid_resources(search_filter, aggregated_res_info_repo, web_src_repo)
 #
 #         elapsedtime.stop_measuring_total()
