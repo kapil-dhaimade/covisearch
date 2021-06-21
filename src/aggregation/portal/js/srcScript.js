a2adb1787ab2df81a670a3ef3ec6682ca5027f06
Deepali Dwivedi <deepali.b.dwivedi@gmail.com>
2021-06-21 23:15:02 +0530

    sources page changes

--- src/aggregation/portal/js/srcScript.js
+++ src/aggregation/portal/js/srcScript.js
@@ -27,19 +27,19 @@ app.controller('srcCtrl', function ($scope) {
         },
         {
             'url': 'http://dashboard.covid19.ap.gov.in/ims/hospbed_reports',
-            'name': 'COVID 19 Andhra Pradesh'
+            'name': 'Covid 19 Andhra Pradesh'
         },
         {
             'url': 'https://coronabeds.jantasamvad.org/index.html',
-            'name': 'COVID-19 Delhi Government'
+            'name': 'Covid-19 Delhi Government'
         },
         {
             'url': 'https://www.justdial.com/verticals/Coronavirus-India/Resources',
-            'name': 'Justdial COVID 19 Resources'
+            'name': 'Justdial Covid 19 Resources'
         },
         {
             'url': 'https://www.divcommpunecovid.com/ccsbeddashboard/hsr',
-            'name': 'COVID CARE SOFTWARE - PUNE DIVISION'
+            'name': 'Covid Care Software - Pune Division'
         },
         {
             'url': 'https://stopcorona.tn.gov.in/beds.php',
